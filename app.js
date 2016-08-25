98bfa69caab8bddd2748945719748e5f063cbd37
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-08-25 14:48:24 -0700

    catch up updates, first pass

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-use strict';
+'use strict';
 
-//Create Store object using constructor notation from literal notation properties
+// Create Store object using constructor notation from literal notation properties
 function Store(name, avg, min, max) {
   this.storeName = name;
   this.avgCookies = avg;
@@ -8,6 +8,7 @@ function Store(name, avg, min, max) {
   this.maxCust = max;
   this.totalCookies = 0;
   this.hoursArray = ['6:00 am', '7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm'];
+  this.hourlyCookies = [];
 }
 
 //Multiple instances of Store object created with values of each new object's properties
@@ -31,82 +32,57 @@ Store.prototype.cookiesPerHour = function() {
 
 //Method for generating number of total cookies sold per day
 Store.prototype.cookiesPerDay = function() {
-  this.hourlyCookies = [];
   for (var i = 0; i < this.hoursArray.length; i++) {
-    this.hourlyCookies.push(this.cookiesPerHour());
-    this.totalCookies += this.cookiesPerHour();
+    var hourlySales = this.cookiesPerHour();
+    this.hourlyCookies.push(hourlySales);
+    this.totalCookies += hourlySales;
   }
 };
 
-//Method for rendering properties as unordered list for each location
+//Method for rendering properties as table with rows for each location data
 Store.prototype.renderData = function() {
-  var ul       = document.createElement('ul');
-  var h2       = document.createElement('h2');
+//Create table
+  var salesTable    = document.createElement('table');
+//Create thead
+  var headerContent = ['', '6:00 am', '7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm'];
+  var salesHead     = document.createElement('thead');
+  var tableRow      = document.createElement('tr');
+  var tableHead     = document.createElement('th');
 
-  h2.textContent = this.storeName;
-
-  ul.appendChild(h2);
-
-  for (var j = 0; j < this.hoursArray.length; j++) {
-    var li = document.createElement('li');
-    li.textContent = this.hoursArray[j] + ': ' + this.hourlyCookies;
-    ul.appendChild(li);
+  for(var k = 0; k < headerContent.length; k++) {
+    var th = document.createElement('th');
+    th.textContent = headerContent[k];
+    tableRow.appendChild(th);
   }
 
-  this.cookiesPerDay();
+  salesHead.appendChild(tableRow);
+  salesTable.appendChild(salesHead);
 
-  var totalLi = document.createElement('li');
-  li.textContent = 'Total: ' + this.totalCookies;
-  ul.appendChild(totalLi);
+  //Create tbody
+  var bodyContent = this.hourlyCookies;
+  var tbody       = document.createElement('tbody');
+  var bodyRow     = document.createElement('tr');
+  var rowHeader   = document.createElement('th');
 
+  rowHeader.textContent = this.storeName;
+  bodyRow.appendChild(rowHeader);
+
+  for(var l = 0; l < bodyContent.length; l++) {
+    var td = document.createElement('td');
+    td.textContent = bodyContent[l];
+    bodyRow.appendChild(td);
+  };
+
+  tbody.appendChild(bodyRow);
+  salesTable.appendChild(tbody);
+
+  //Add sales table to the DOM
   var main = document.getElementById('store_info');
-  main.appendChild(ul);
+  main.appendChild(salesTable);
 };
-
-console.log(pike, seaTac, seaCenter, capHill, alki);
 
 pike.renderData();
 seaTac.renderData();
 seaCenter.renderData();
 capHill.renderData();
 alki.renderData();
-
-//Create sales table
-var salesTable    = document.createElement('table');
-
-//Create thead
-var headerContent = ['', '6:00 am', '7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm'];
-var salesHead     = document.createElement('thead');
-var tableRow      = document.createElement('tr');
-var tableHead     = document.createElement('th');
-
-for(var k = 0; k < headerContent.length; k++) {
-  var th = document.createElement('th');
-  th.textContent = headerContent[k];
-  tableRow.appendChild(th);
-}
-
-salesHead.appendChild(tableRow);
-salesTable.appendChild(salesHead);
-
-//Create tbody
-var bodyContent = [this.cookiesPerHour];
-var tbody       = document.createElement('tbody');
-var bodyRow     = document.createElement('tr');
-var rowHeader   = document.createElement('th');
-
-rowHeader.textContent = this.storeName;
-bodyRow.appendChild(rowHeader);
-
-for(var l = 0; l < bodyContent.length; l++) {
-  var td = document.createElement('td');
-  td.textContent = bodyContent;
-  bodyRow.appendChild(td);
-};
-
-tbody.appendChild(bodyRow);
-salesTable.appendChild(tbody);
-
-//Add sales table to the DOM
-var main = document.getElementById('store_info');
-main.appendChild(salesTable);
