ec898970859b5659c8b2a625001dcd5fd414908f
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-08-25 09:36:46 -0700

    changes made for eventual re-submit

--- app.js
+++ app.js
@@ -1,113 +1,112 @@
-'use strict';
+use strict';
 
+//Create Store object using constructor notation from literal notation properties
 function Store(name, avg, min, max) {
   this.storeName = name;
   this.avgCookies = avg;
   this.minCust = min;
   this.maxCust = max;
+  this.totalCookies = 0;
+  this.hoursArray = ['6:00 am', '7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm'];
 }
 
-Store.prototype.generateRandom = function() {
-  return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-};
-
-Store.prototype.cookiesPerHour = function() {
-  return this.generateRandom() * this.avgCookies;
-};
-
-Store.prototype.cookiesPerDay = function() {
-  for (var i = 0; i < 14; i++) {
-    this.hoursArray[i][1] = Math.floor(this.cookiesPerHour());
-    this.totalCookies += this.hoursArray[i][1];
-  }
-  this.hoursArray[(this.hoursArray.length - 1)][1] = this.totalCookies;
-};
-
+//Multiple instances of Store object created with values of each new object's properties
 var pike = new Store('Pike Place', 6.3, 23, 65);
 var seaTac = new Store('SeaTac', 1.2, 3, 24);
 var seaCenter = new Store('Seattle Center', 3.7, 11, 38);
 var capHill = new Store('Capitol Hill', 2.3, 20, 38);
 var alki = new Store('Alki', 4.6, 2, 16);
 
-var salesTable = document.createElement('table');
+//Create prototype of Store object (allows method of object to be applied in more than one instance and save memory)
 
-//creating thead
-var headerContent = ['', '6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', 'Daily Location Total'];
+//Method for generating random number to be used in calculating number of cookies sold per hour (using avg, min, max for each store)
+Store.prototype.generateRandom = function() {
+  return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+};
+
+//Method for generating number of cookies sold per hour
+Store.prototype.cookiesPerHour = function() {
+  return Math.floor(this.generateRandom() * this.avgCookies);
+};
+
+//Method for generating number of total cookies sold per day
+Store.prototype.cookiesPerDay = function() {
+  this.hourlyCookies = [];
+  for (var i = 0; i < this.hoursArray.length; i++) {
+    this.hourlyCookies.push(this.cookiesPerHour());
+    this.totalCookies += this.cookiesPerHour();
+  }
+};
+
+//Method for rendering properties as unordered list for each location
+Store.prototype.renderData = function() {
+  var ul       = document.createElement('ul');
+  var h2       = document.createElement('h2');
+
+  h2.textContent = this.storeName;
+
+  ul.appendChild(h2);
+
+  for (var j = 0; j < this.hoursArray.length; j++) {
+    var li = document.createElement('li');
+    li.textContent = this.hoursArray[j] + ': ' + this.hourlyCookies;
+    ul.appendChild(li);
+  }
+
+  this.cookiesPerDay();
+
+  var totalLi = document.createElement('li');
+  li.textContent = 'Total: ' + this.totalCookies;
+  ul.appendChild(totalLi);
+
+  var main = document.getElementById('store_info');
+  main.appendChild(ul);
+};
+
+console.log(pike, seaTac, seaCenter, capHill, alki);
+
+pike.renderData();
+seaTac.renderData();
+seaCenter.renderData();
+capHill.renderData();
+alki.renderData();
+
+//Create sales table
+var salesTable    = document.createElement('table');
+
+//Create thead
+var headerContent = ['', '6:00 am', '7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm'];
 var salesHead     = document.createElement('thead');
 var tableRow      = document.createElement('tr');
+var tableHead     = document.createElement('th');
 
-for(var i = 0; i < headerContent.length; i++) {
+for(var k = 0; k < headerContent.length; k++) {
   var th = document.createElement('th');
-  th.textContent = headerContent[i];
+  th.textContent = headerContent[k];
   tableRow.appendChild(th);
 }
 
 salesHead.appendChild(tableRow);
 salesTable.appendChild(salesHead);
 
-//creating tbody
-var bodyContent = [];
+//Create tbody
+var bodyContent = [this.cookiesPerHour];
 var tbody       = document.createElement('tbody');
 var bodyRow     = document.createElement('tr');
 var rowHeader   = document.createElement('th');
 
-rowHeader.textContent = 'Pike Place';
+rowHeader.textContent = this.storeName;
 bodyRow.appendChild(rowHeader);
 
-for(var i = 0; i < bodyContent.length; i++) {
+for(var l = 0; l < bodyContent.length; l++) {
   var td = document.createElement('td');
-  td.textContent = bodyContent[i];
+  td.textContent = bodyContent;
   bodyRow.appendChild(td);
 };
 
 tbody.appendChild(bodyRow);
 salesTable.appendChild(tbody);
 
-//adding table to dom
-var main = document.getElementById('sales_data');
+//Add sales table to the DOM
+var main = document.getElementById('store_info');
 main.appendChild(salesTable);
-
-  //Renders number of cookies sold per each hour of operation and total cookies sold per day for store location
-//   render: function() {
-//     var h2 = document.createElement('h2');
-//     var ul = document.createElement('ul');
-//     var li = document.createElement('li');
-//
-//     this.cookiesPerDay();
-//     h2.textContent = 'Pike';
-//     li.textContent = 'Total: ' + this.totalCookies;
-//
-//     ul.appendChild(h2);
-//     ul.appendChild(li);
-//
-//     var main = document.getElementById('store_info');
-//     main.appendChild(ul);
-//
-//     for (var j = 0; j < 14; j++) {
-//       var hourLi = document.createElement('li');
-//       hourLi.textContent = this.hoursArray[j][0] + ': ' + this.hoursArray[j][1];
-//       ul.appendChild(hourLi);
-//     }
-//   }
-// };
-// pike.cookiesPerDay();
-// pike.render();
-
-// //Function to display number of cookies sold per each hour of operation as nested array
-// function displayHours() {
-//   var hour;
-//   var hoursArray = [];
-//   for(var i = 0; i <= 14; i++) {
-//     if (i < 6) {
-//       hour = i + 6 + ' AM';
-//     } else if (i === 6){
-//       hour = '12 PM';
-//     } else if (i < 14) {
-//       hour = i - 6 + ' PM';
-//     } else {
-//       hour = 'Total: ';
-//     }
-//     hoursArray[i] = [hour];
-//   }
-//   return hoursArray;
-// };
